5041cb2d86a53947fe03fe14e0d402b0eaa2d3c6
developers-codz <kaj123al2002@gmail.com>
2022-05-28 08:56:42 +0530

    feat: added landing and rules page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Navbar,Footer } from './components';
+import { Landing ,Rules} from './pages';
 import './App.css';
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/rules" element={<Rules />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
